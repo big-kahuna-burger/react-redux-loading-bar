2dcf805e631b7f74413c4c087c0717c373612734
big-kahuna-burger <arandjel.sarenac@hotmail.co.uk>
2018-12-27 07:19:05 +0100

    Allow me to typeProp

--- src/loading_bar_middleware.js
+++ src/loading_bar_middleware.js
@@ -2,26 +2,28 @@ import { DEFAULT_SCOPE, showLoading, hideLoading } from './loading_bar_ducks'
 
 const defaultTypeSuffixes = ['PENDING', 'FULFILLED', 'REJECTED']
 
-export default function loadingBarMiddleware(config = {}) {
+export default function loadingBarMiddleware (config = { typeProp: 'type' }) {
+  const typeProp = config.typeProp
   const promiseTypeSuffixes = config.promiseTypeSuffixes || defaultTypeSuffixes
   const scope = config.scope || DEFAULT_SCOPE
 
-  return ({ dispatch }) => next => (action) => {
-    if (action.type) {
+  return ({ dispatch }) => next => action => {
+    if (action[typeProp]) {
       const [PENDING, FULFILLED, REJECTED] = promiseTypeSuffixes
 
       const isPending = new RegExp(`${PENDING}$`, 'g')
       const isFulfilled = new RegExp(`${FULFILLED}$`, 'g')
       const isRejected = new RegExp(`${REJECTED}$`, 'g')
 
-      const actionScope = (action.meta && action.meta.scope) ||
-                          action.scope ||
-                          scope
+      const actionScope =
+        (action.meta && action.meta.scope) || action.scope || scope
 
-      if (action.type.match(isPending)) {
+      if (action[typeProp].match(isPending)) {
         dispatch(showLoading(actionScope))
-      } else if (action.type.match(isFulfilled) ||
-                 action.type.match(isRejected)) {
+      } else if (
+        action[typeProp].match(isFulfilled) ||
+        action[typeProp].match(isRejected)
+      ) {
         dispatch(hideLoading(actionScope))
       }
     }
